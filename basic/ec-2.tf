7243d497f4a8688ec3c2326ea428ca1eba48db79
oleksandr-yakov <alex.yakov1109@gmail.com>
2024-10-03 21:52:58 +0300

    simple creating ec2 using terraform

---
+++ basic/ec-2.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region     = "eu-central-1"
+}
+
+resource "aws_instance" "ubuntu" {
+    count         = 1
+    ami           = "ami-0e04bcbe83a83792e"
+    instance_type = "t3.micro"
+    tags = {
+        Name    = "basic  ubuntu"
+        Owner   = "Alex"
+        Project = "Terraform basic project"
+    }
+}
+
+resource "aws_instance" "amz" {
+    ami           = "ami-00f07845aed8c0ee7"
+    instance_type = "t3.micro"
+        tags = {
+        Name    = "basic  amz"
+        Owner   = "Alex"
+        Project = "Terraform basic project"
+    }
+}
