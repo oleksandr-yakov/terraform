e2022afffba31774e64029cc49bdc6c73f6386e9
oleksandr-yakov <alex.yakov1109@gmail.com>
2024-10-07 14:37:08 +0300

    add new formating and create sg with best pract

--- web-server-simple/infra-web-server.tf
+++ web-server-simple/infra-web-server.tf
@@ -3,31 +3,34 @@ provider "aws" {
 }
 
 resource "aws_security_group" "web-server-sg" {
-  name = "basic-sg"
+  name        = "basic-sg"
   description = "Allow inbound HTTP traffic from the world"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # any protocol
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "allow_tls"
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "Allow SSH traffic from the world" {
+  security_group_id = aws_security_group.web-server-sg.id
+  ip_protocol       = "tcp"
+  from_port         = 22
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "Allow HTTP traffic from the world" {
+  security_group_id = aws_security_group.web-server-sg.id
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "Allow Allow Egress traffic from the world" {
+  security_group_id = aws_security_group.web-server-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+
+}
 
 resource "tls_private_key" "generation-key" {
   algorithm = "RSA"
@@ -41,7 +44,7 @@ resource "aws_key_pair" "main-key-id" {
 
 output "private_key_pem" {
   value     = tls_private_key.generation-key.private_key_pem
-  sensitive = true  
+  sensitive = true
 }
 
 output "ec2_public_ip" {
@@ -52,11 +55,11 @@ output "ec2_public_ip" {
 # resource "aws_eip" "elastic-for-web" {
 #   instance = aws_instance.web-server-ec2.id
 #   domain   = "vpc"
-  
+
 # }
 
 resource "aws_eip" "elastic_ip" {
-  domain   = "vpc"
+  domain = "vpc"
 }
 
 resource "aws_eip_association" "eip_assoc" {
@@ -70,11 +73,11 @@ output "elastic_ip" {
 }
 
 resource "aws_instance" "web-server-ec2" {
-    ami = "ami-00f07845aed8c0ee7"
-    instance_type = "t3.micro" 
-    key_name      = aws_key_pair.main-key-id.key_name
-    vpc_security_group_ids = [aws_security_group.web-server-sg.id]  
-  user_data_replace_on_change = true   # This need to added!!!!  
+  ami                         = "ami-00f07845aed8c0ee7"
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.main-key-id.key_name
+  vpc_security_group_ids      = [aws_security_group.web-server-sg.id]
+  user_data_replace_on_change = true # This need to added!!!!  
   user_data                   = <<EOF
 #!/bin/bash
 yum -y update
@@ -84,9 +87,9 @@ echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!"  >  /var/www/htm
 sudo service httpd start
 chkconfig httpd on
 EOF
-    tags = {
-      Name = "web-server-ec2"
-    }
+  tags = {
+    Name = "web-server-ec2"
+  }
 }
 
 
