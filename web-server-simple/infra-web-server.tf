4c43ece5be209b2b5941f041c5c1f5dc34bfe644
oleksandr-yakov <alex.yakov1109@gmail.com>
2024-10-07 20:43:11 +0300

    using depens_on

--- web-server-simple/infra-web-server.tf
+++ web-server-simple/infra-web-server.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "web-server-sg" {
     }
   }
   tags = {
-    Name = "Terraform",
+    Name  = "Terraform",
     Ovner = "Alex Yakov"
   }
 }
@@ -39,12 +39,6 @@ resource "aws_key_pair" "main-key-id" {
   public_key = tls_private_key.generation-key.public_key_openssh
 }
 
-# resource "aws_eip" "elastic-for-web" {
-#   instance = aws_instance.web-server-ec2.id
-#   domain   = "vpc"
-
-# }
-
 #====================Elastic IP =================
 resource "aws_eip" "elastic_ip" {
   domain = "vpc"
@@ -55,6 +49,13 @@ resource "aws_eip_association" "eip_assoc" {
   allocation_id = aws_eip.elastic_ip.id
 }
 
+# resource "aws_eip" "elastic-for-web" {
+#   instance = aws_instance.web-server-ec2.id
+#   domain   = "vpc"
+
+# }
+
+#=====================EC2========================
 resource "aws_instance" "web-server-ec2" {
   ami                         = "ami-00f07845aed8c0ee7"
   instance_type               = "t3.micro"
@@ -83,27 +84,33 @@ resource "aws_instance" "web-server-ec2" {
   }
 
   lifecycle {
-    
-              /*prevent_destroy забороняє видаляти ресурс, навіть якщо він є у плані на видалення.
+
+    /*prevent_destroy забороняє видаляти ресурс, навіть якщо він є у плані на видалення.
               Це важливо для критичних ресурсів, які не повинні випадково бути знищені.*/
     # prevent_destroy = true
 
-              /*ignore_changes ігнорує зміни у вказаних полях під час наступних запусків.
+    /*ignore_changes ігнорує зміни у вказаних полях під час наступних запусків.
               Terraform не буде намагатися перезапустити або змінити інстанс через ці зміни.*/
     # ignore_changes = [ "ami", "instance_type", "user_data" ]
 
-              /*create_before_destroy змушує Terraform спочатку створити новий ресурс, а тільки потім видалити старий.
+    /*create_before_destroy змушує Terraform спочатку створити новий ресурс, а тільки потім видалити старий.
               Це корисно для ресурсів, які не повинні зникати під час оновлення (наприклад, важливі сервіси чи інстанси).
               Terraform створить новий ресурс, перенесе до нього всі залежності, і лише після цього знищить старий ресурс.*/
     create_before_destroy = true
-    
+
   }
 }
 
+resource "aws_instance" "data_base" {
+  ami           = "ami-00f07845aed8c0ee7"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.main-key-id.key_name
+  depends_on = [ aws_instance.web-server-ec2 ]
+}
 
 #====================Secret Manager =================
 resource "aws_secretsmanager_secret" "ec2_private_key_secret" {
-  name = "ec2-key-web-serve1"
+  name = "ec2-key-web-serv-secret"
 }
 
 resource "aws_secretsmanager_secret_version" "ec2_private_key_version" {
